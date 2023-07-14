a175aa708fcd3e99cee05df790b877073ee23982
norviah <21983700+Norviah@users.noreply.github.com>
2023-07-13 21:19:39 -0700

    feat: implement an option to specify a root directory when running a shell script

--- src/util/run.ts
+++ src/util/run.ts
@@ -7,11 +7,14 @@ import type { SpawnSyncReturns } from 'child_process';
  * Executes a shell script and returns the output.
  *
  * @param command The command to execute.
+ * @param options Options for the script execution.
  * @returns The output of the command.
  */
-export function run(command: string): string {
+export function run(command: string, options?: { root?: string }): string {
+  const script: string = `${options?.root ? `cd ${options.root} && ` : ''}${command} 2>/dev/null`;
+
   try {
-    return execSync(`${command} 2>/dev/null`, { encoding: 'utf-8' }).toString().trim();
+    return execSync(script, { encoding: 'utf-8' }).toString().trim();
   } catch (error) {
     throw new BumpError(ErrorCodes.SCRIPT_ERROR, error as SpawnSyncReturns<string>);
   }
