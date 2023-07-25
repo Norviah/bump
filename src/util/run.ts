38a3a823acf2c4252268a97521350cd755ba9cd3
Norviah <21983700+Norviah@users.noreply.github.com>
2023-07-25 00:27:02 -0700

    feat(deps): add the dependency `execa` for running shell commands

--- src/util/run.ts
+++ src/util/run.ts
@@ -1,21 +1,26 @@
 import { BumpError, ErrorCodes } from '@/structs/BumpError';
-import { execSync } from 'child_process';
+import { command as exec } from 'execa';
 
-import type { SpawnSyncReturns } from 'child_process';
+import type { ExecaError } from 'execa';
 
 /**
  * Executes a shell script and returns the output.
  *
  * @param command The command to execute.
  * @param options Options for the script execution.
+ * @param timeout Timeout in milliseconds after which the command will be
+ * terminated.
  * @returns The output of the command.
  */
-export function run(command: string, options?: { root?: string }): string {
-  const script: string = `${options?.root ? `cd ${options.root} && ` : ''}${command} 2>/dev/null`;
-
+export async function run(command: string, options?: { root?: string; timeout: number }): Promise<string> {
   try {
-    return execSync(script, { encoding: 'utf-8' }).toString().trim();
+    const response = await exec(command, {
+      cwd: options?.root,
+      timeout: options?.timeout,
+    });
+
+    return `${response.stdout.trim()}\n${response.stderr.trim()}`.trim();
   } catch (error) {
-    throw new BumpError(ErrorCodes.SCRIPT_ERROR, error as SpawnSyncReturns<string> & Error);
+    throw new BumpError(ErrorCodes.SCRIPT_ERROR, error as ExecaError);
   }
 }
